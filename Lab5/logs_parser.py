071ef9a7efd7064091afe43885e67e24f33c0cb1
nick0der <mykolarpopyk@gmail.com>
2021-07-21 18:06:54 +0300

    new update

--- Lab5/logs_parser.py
+++ Lab5/logs_parser.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import time
+from flask_sqlalchemy import sqlalchemy
 from datetime import datetime
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
 
